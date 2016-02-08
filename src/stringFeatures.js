4716e7a5508130c218935fbb523e86a646be864b
dlinaz <dlina3266@gmail.com>
2016-02-08 07:49:21 +0000

    (feat) adding examples for New string features

---
+++ src/stringFeatures.js
@@ -0,0 +1,31 @@
+"use strict"
+
+
+// String interpolation: supports string interpolation via template literals
+
+
+let first = 'Jon';
+let last = 'Smith';
+console.log(`Hello ${first} ${last}!`);  // Hello Jon Smith
+
+
+// Multi-line string
+
+const multiLine = `
+This is
+a string
+with four
+lines`;
+
+console.log (multiLine);
+
+// This is
+// a string
+// with four
+// lines
+
+
+
+
+
+
